3ca83509a7d6ac31e3208bbc49e91730c776939f
Alessio Rigamonti <alessioriga93@hotmail.it>
2025-04-01 13:56:38 +0100

    Update repository.py

--- db/resources/repository.py
+++ db/resources/repository.py
@@ -132,3 +132,62 @@ def deleteDonor ():
     else:
         print("❌ The Donor hasn't been deleted.")
         time.sleep(2)
+
+def insertNewPaymentMethod ():
+
+    connection = db.connect("Alessio_charity_donations.db")
+
+    method = input_mandatory("Insert the Payment Method: ")
+
+    connection.execute("INSERT INTO PaymentMethod (method) VALUES (?)", (method))
+    connection.commit()
+    connection.close()
+
+    print("✅ Pyment Method added.")
+    time.sleep(2)
+
+def viewPaymentMethods ():
+
+    connection = db.connect("Alessio_charity_donations.db")
+
+    print("Payment Methods:\n")
+    for row in connection.execute("SELECT * FROM PaymentMethod"):
+        print(row)
+
+    connection.close()
+    time.sleep(2)
+
+def updatePaymentMethods ():
+
+    connection = db.connect("Alessio_charity_donations.db")
+
+    print("Insert a Payment Method ID to update.\n")
+    paymentMethod_id = paymentMethodIdInput(connection)
+    updated_method = input_mandatory("Insert the new Payment Method: ")
+
+    connection.execute("UPDATE PaymentMethod SET method = ? WHERE id = ?", (updated_method, paymentMethod_id))
+
+    connection.commit()
+    connection.close()
+
+    print("✅ Payment Method updated.")
+    time.sleep(2)
+
+def deletePaymentMethod ():
+    connection = db.connect("Alessio_charity_donations.db")
+    
+    cursor = connection.cursor()
+
+    print("Insert a Payment Method ID to delete.\n")
+    paymentMethod_id = paymentMethodIdInput(connection)
+    if confirmation_input("Are you sure you want to delete Payment method ID " + paymentMethod_id + "?"):
+        if searchDonationByPaymentMethodId(connection, paymentMethod_id):
+            print("❗ This payment method ID is being used by a donation.")
+        else:
+            cursor.execute("DELETE FROM PaymentMethod WHERE id = ?", (paymentMethod_id))
+            connection.commit()
+            print("✅ Payment Method: " + paymentMethod_id + " has been deleted.")
+        time.sleep(2)
+    else:
+        print("❌ The Payment Method hasn't been deleted.")
+        time.sleep(2)
